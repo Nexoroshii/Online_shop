3040eb85235f8fd5e7eba9963776e65b89b2a8f1
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-04 13:58:57 +0300

    create controllers

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
 
-const PORT = process.env.PORT || 5000; //запуск на порте
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('./api', router);
+app.use('/api', router);
 
 const start = async () => {
     try {
